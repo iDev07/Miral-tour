be2623c04664f6edece9e11657deaa388e9831b6
Diyorbek <idevdiyor@gmail.com>
2024-01-24 23:04:27 +0500

    d

--- miral-tour/src/components/Mobile/header.jsx
+++ miral-tour/src/components/Mobile/header.jsx
@@ -7,7 +7,8 @@ import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
 import { useAppSelector } from "../../../store/store";
 import { Container } from "@mui/material";
-import { KeyboardArrowDown } from "@mui/icons-material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { KeyboardArrowDown, ShoppingCart } from "@mui/icons-material";
 const DropdownContainer = styled.div`
   width: 100%;
   position: relative;
@@ -164,18 +165,7 @@ function MobileHeader() {
             </div>
             <div className="cart">
               <Link href={"/cart"}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M12 21.35L10.55 20.03C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5C22 12.27 18.6 15.36 13.45 20.03L12 21.35Z"
-                    fill="#0073DB"
-                  />
-                </svg>
+                <ShoppingCartIcon />
                 <span id="qty">{cartQuantity}</span>
               </Link>
             </div>
