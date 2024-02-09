4ae12de7d43599c9eef9a908260d63d293d05f92
Diyorbek <idevdiyor@gmail.com>
2024-02-09 17:59:47 +0500

    sitemap added

--- miral-tour/src/pages/destinations/index.js
+++ miral-tour/src/pages/destinations/index.js
@@ -1,30 +1,38 @@
 import Loader from "@/components/Loader";
 import { ChevronRight } from "@mui/icons-material";
 import { Container } from "@mui/material";
+import { Select } from "antd";
 import axios from "axios";
 import Head from "next/head";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
-function Cities() {
-  const [cities, setCities] = useState([]);
+function Destinations() {
+  const [destinations, setDestinations] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios
-      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/destinations`)
-      .then((response) => {
-        setCities(response.data);
-        setLoading(false);
-      })
+      .all([
+        axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/cities`),
+        axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/destinations`),
+      ])
+      .then(
+        axios.spread((citiesResponse, destinationsResponse) => {
+          setLoading(false);
+          setCities(citiesResponse.data);
+          setDestinations(destinationsResponse.data);
+        })
+      )
       .catch((error) => {
         console.error("An error occurred:", error);
         setLoading(false);
       });
   }, []);
+
   return (
     <>
       <Head>
-        <title>Uzbekistan cities and regions Miral Tour Uzbekistan</title>
+        <title>Uzbekistan destinations and regions Miral Tour Uzbekistan</title>
       </Head>
       {loading ? (
         <Loader />
@@ -33,21 +41,24 @@ function Cities() {
           <div className="all_places_destinations pt_100">
             <div className="all_places_wrapper">
               <div className="intro">
-                <h1>Uzbekistan cities and regions</h1>
+                <div className="title">
+                  <h1>Uzbekistan destinations and regions</h1>
+                </div>
+                <div className="sort"></div>
               </div>
               <div className="places">
-                {cities &&
-                  cities.length &&
-                  cities.map((city) => (
+                {destinations &&
+                  destinations.length &&
+                  destinations.map((destination) => (
                     <div className="place">
-                      <Link href={`/destinations/${city.seo_url}`}>
+                      <Link href={`/destinations/${destination.seo_url}`}>
                         <img
-                          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/destination/${city.main_image}`}
-                          alt={`${city.title_en}`}
+                          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/destination/${destination.main_image}`}
+                          alt={`${destination.title_en}`}
                         />
                         <div className="place_wrap">
                           <div className="content">
-                            <h2>{city.title_en}</h2>
+                            <h2>{destination.title_en}</h2>
                             <p className="read_more">
                               Read more{" "}
                               <span>
@@ -68,4 +79,4 @@ function Cities() {
   );
 }
 
-export default Cities;
+export default Destinations;
